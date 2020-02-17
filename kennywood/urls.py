ffde90ff21f2616126a957ad16fdfa4c749e9eac
John Long <jlcomp45@gmail.com>
2020-02-17 15:18:59 -0600

    completed models and migrations for kennywood

--- kennywood/urls.py
+++ kennywood/urls.py
@@ -19,6 +19,8 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 router = routers.DefaultRouter(trailing_slash=False)
 
+from kennywoodapi.models import *
+
 urlpatterns = [
     path('', include(router.urls)),
     path('api-token-auth/', obtain_auth_token),
